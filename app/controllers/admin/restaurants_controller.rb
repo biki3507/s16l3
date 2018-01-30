fd6a675ac309c2ad2445dabe5fa7b4f6d18fc810
David <biki3507@gmail.com>
2018-01-31 03:05:48 +0800

    S15 L2 U8 add Admin::BaseController and move authenticate_admin here

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,4 @@
-class Admin::RestaurantsController < ApplicationController
-    before_action :authenticate_admin
+class Admin::RestaurantsController < Admin::BaseController
     before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
     def index
@@ -50,13 +49,7 @@ class Admin::RestaurantsController < ApplicationController
     def restaurant_params
       params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image, :category_id)
     end
-
-    def authenticate_admin
-      unless current_user.admin?
-        flash[:alert] = 'Not allow!'
-        redirect_to root_path
-      end
-    end
+    
     def set_restaurant
       @restaurant = Restaurant.find(params[:id])
     end
