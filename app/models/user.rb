4286b1e13d52af55180393ce4dc2190ed58e9a64
David <biki3507@gmail.com>
2018-01-31 11:19:26 +0800

    S16 L2 U1

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
         end
 
         has_many :comments, dependent: :restrict_with_error
+          # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
 end
