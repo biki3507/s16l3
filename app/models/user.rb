ba05f031b0c9a21f0ba61d525b311dca5d31eee2
David <biki3507@gmail.com>
2018-01-31 11:38:25 +0800

    S16 L3 U1

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ApplicationRecord
           # 「使用者收藏很多餐廳」的多對多關聯
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
 end
